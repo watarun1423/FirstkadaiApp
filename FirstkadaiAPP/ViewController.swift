aa553212af1d516c97ed7149d99d93c558d65f4c
Wataru Tujimoto <wataru1423@icloud.com>
2018-10-26 23:37:08 +0900

    Initial Commit

---
+++ FirstkadaiAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstkadaiAPP
+//
+//  Created by 辻本亘 on 2018/10/26.
+//  Copyright © 2018 wataru.tujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
